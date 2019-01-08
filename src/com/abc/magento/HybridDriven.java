aef3a3acd0064dce75b17d53247c4f9a2fd24e0c
vani <vani@vani-pavani>
2019-01-08 22:18:06 +0530

    Hybrid driven framework

---
+++ src/com/abc/magento/HybridDriven.java
@@ -0,0 +1,32 @@
+package com.abc.magento;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class HybridDriven
+{
+	public static void main(String[] args) throws IOException 
+	{
+  		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/libs/chromedriver");
+		InputStream file = HybridDriven.class.getClassLoader().getResourceAsStream("com/abc/magento/hybrid.properties");
+		Properties p=new Properties();
+		p.load(file);
+		String url=p.getProperty("url");
+		String emailloc=p.getProperty("emailloc");
+		String email=p.getProperty("email");
+		String passwordlocation=p.getProperty("passloc");
+		String password=p.getProperty("pass");
+		String next=p.getProperty("next");
+		ChromeDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+	    driver.get(url);
+	    driver.findElement(By.linkText("My Account")).click();
+	    driver.findElement(By.xpath(emailloc)).sendKeys(email);
+	    driver.findElement(By.xpath(passwordlocation)).sendKeys(password);
+	    driver.findElement(By.xpath(next)).click();
+	    driver.findElement(By.linkText("Log Out")).click();
+	}
+}
